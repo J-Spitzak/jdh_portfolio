ff1aa66d4aa51b4ed6d9fab9e1c71ca72a6a8e39
Jason Spitzak <jason.spitzak@gmail.com>
2026-06-30 01:08:19 -0400

    working on some stuff

--- sim-src/main.js
+++ sim-src/main.js
@@ -36,6 +36,7 @@
 testDrawing = new JDHDrawing( "testDraw" );
 bg = new Frame( 0, 0, 1, 1 );
 //bg.setCombinedPaint( rgba(247, 6, 6,.5) );
+bg.setBackgroundPaint( rgb(250,250,250) );
 testDrawing.add( bg );
 ////////////////////////////////////////////
 //// setting up scrollbar  ////////////////////
@@ -51,10 +52,29 @@ rect = new Rectangle(10,800,10,500);
 rect.setLineWidth( 10 );
 MainScroll.add(rect);
 
+////////////////////////Name and Picture///////////////////
+startY = 0;
+
+nameTile = new Text(.2,400,"Jason Spitzak");
+nameTile.setAlignment(ALIGN_ABOVE_LEFT);
+nameTile.setFontSize(42);
 
 
-//// input fields   /////////////////////////////////////
+img = new ImageRectangle( .7, .1, 400, 400 );
+img.source( "./Me.jpg" );
+
+Namebkg = new Rectangle(0,0,1,700);
+rect2.setBackgroundPaint( rgba( .5, .5, .2 ,.05) );
+Namebkg.setLineWidth( 0 );
+
+MainScroll.add(Namebkg);
+MainScroll.add(nameTile);
+MainScroll.add(img);
+
+
 /////////////////////////////////////////////////////////
+startY = 700;
+
 
 
 
