3770e2b80df77437ecc2d7ba71e2e5a5cb1e27bb
Jason Spitzak <jason.spitzak@gmail.com>
2026-08-07 16:29:32 -0400

    got text boxes formatted correctly

--- sim-src/main.js
+++ sim-src/main.js
@@ -9,8 +9,8 @@
 
 //////////// text
 
-var AboutMain = "I am a second year Mechanical Engineering student at the Olin College of Engineering. I love working on projects that require building something that should require far more time, experience and budget than I really have. Last Semester I finished a fully mechanical air raid siren. I will soon start building a stirling cryocooler and I am working out the control system for a omnicopter drone. I also have a passion for physics and nanotechnology.";
-var EducationMain = "I am currently in my second year studying Mechanical Engineering at Olin College. Olin is a small engineering-focused school in Needham Massachusetts known for it's modern approach to engineering education. Prior to Olin I went to school at the Arlington Tech program in the Arlington Career Center (now Grace Hopper Center)."
+var AboutMain = "I am a second year Mechanical Engineering student <@nat the Olin College of Engineering.  I love working on <@nprojects that require building something that should  <@nrequire far more time, experience and budget than <@nI really have. Last Semester I finished a fully <@nmechanical air raid siren. I will soon start building a <@nstirling cryocooler and I am working out the control <@nsystem for a omnicopter drone. I also have a <@npassion for physics and nanotechnology.";
+var EducationMain = "I am currently in my second year studying <@nMechanical Engineering at Olin College. Olin is a small <@nengineering-focused school in Needham Massachusetts <@nknown for it's modern approach to engineering <@neducation. Prior to Olin I went to school at the <@nArlington Tech program in the Arlington Career Center <@n(now Grace Hopper Center)."
 var AHSMain = "In my Junior year of high school I was part of a group tasked with creating plaques for the Arlington Historical Society (AHS). AHS wanted to commemorate 3 slaves who had dies at the Ball Sellers House, a historic Arlington landmark. We machined the plaques out of bronze, going through many iterations and being in constant communication with the AHS team about their requirements. I was mostly in charge of the machining and automation of the procedure. Automation was critical as although we initially made 3 it laid the groundwork for AHS's next contract of over 50 plaques to be placed in the ground and over a hundred trophy's given to students by the school."
 var ARSMain = "An Air Raid Siren is a type of siren that uses a circular fan with a matching amount of blades and baffles such that it creates cyclical pressure waves while turning. To produce an audible amount of noise (>~150 Hz) the blades have to spin increadibly fast. Under drill power my rotor rotates 14 times a second and reaches a surface speed of 11.5 m/s. But the whole idea behind this was for it to be entirely mechanical so it can also be powered with a crank, however I never got it to produce a significant audible noise by crank power. Designing a system made out of plywood to be able to withstand the speeds it is subjected to was a huge engineering challenge."
 var NextProjMain = "I am currently working on some upcoming projects that I hope to finish by the end of the next semester. I have designed a stirling cooler that I hope could reach cryogenic temperatures. I am also working on an omnicopter (pircture left). An omnicopter is a type of drone capable of moving and rotating independently of it's position and orientation."
@@ -51,10 +51,12 @@ textComponent.setAlignment(ALIGN_CENTERED_MIDDLE);
 textComponent.setBackgroundPaint(rgba(0,0,0,0));
 RightTile.getFrame2().add(textComponent);
 
-RightText = new TextOutput(0,0,1,1);
+RightText = new Text(.5,.5,AboutMain);
 RightText.setAlignment(ALIGN_CENTERED_MIDDLE);
 RightText.setBackgroundPaint(rgba(0,0,0,0));
 textComponent.add(RightText);
+RightText.setBackground( null );
+RightText.setText(AboutMain);
 
 function changeText(arg) {
     console.log(arg);
@@ -66,10 +68,11 @@ RtextComponent.setAlignment(ALIGN_CENTERED_MIDDLE);
 RtextComponent.setBackgroundPaint(rgba(0,0,0,0));
 RightTile.getFrame2().add(RtextComponent);
 
-RightTextHeader = new TextOutput(0,0,1,1);
+RightTextHeader = new Text(.5,.5, "Hi");
 RightTextHeader.setAlignment(ALIGN_CENTERED_MIDDLE);
 RtextComponent.add(RightTextHeader);
 RightTextHeader.setFontSize(60);
+RightTextHeader.setBackground( null );
 
 //// setting up text ////////////////////////////////////////////
 ///////////////////////// setting up menu ///////////////////////
